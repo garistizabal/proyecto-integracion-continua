7c769d38302fdde368ab239d83ff82642c9ece24
dhmurillo01 <dhmurillo@poligran.edu.co>
2024-06-08 20:10:48 -0500

    Aporte Dos Duvan

--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 
 import {
   createTodo,
+  getOneTodo,
   getTodos,
+  updateTodo,
 } from '../controllers/todo.controller';
 import { schemaValidation } from '../middlewares/schemaValidation';
 import {
   createTodoSchema,
+  getOneTodoSchema,
+  updateTodoSchema,
 } from '../schemas/todo.schemas';
 
 const router = Router();
@@ -14,5 +18,9 @@ const router = Router();
 router.get('/', getTodos);
 
 router.post('/', schemaValidation(createTodoSchema), createTodo);
+
+router.get('/:todoId', schemaValidation(getOneTodoSchema), getOneTodo);
+
+router.put('/:todoId', schemaValidation(updateTodoSchema), updateTodo);
 
 export default router;
